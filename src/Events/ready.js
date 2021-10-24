a8e18a28df03499ac62cd79fcb31af319e5dc139
Tan Shi Yu <tanshiyu1999@gmail.com>
2021-10-24 20:00:41 +0800

    Add bracket functionality

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -1,5 +1,13 @@
 const Event = require("../Structures/Event.js");
+const fs = require("fs");
+
 
 module.exports = new Event("ready", (client) => {
+  if (!fs.existsSync("src/Data/lobbyData.json")) {
+    let brackets = [];
+    fs.writeFileSync('src/Data/lobbyData.json', JSON.stringify(brackets))
+  }
+
+
   console.log("Bot is ready.")
 })
